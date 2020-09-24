5ca9c84d8cef0a5e88f0b64f28878b6cb2d79e13
Alex Yanovich <alexyanovich@Alexs-MacBook-Pro.local>
2020-09-23 21:22:57 -0400

    stores data

--- beers_front_end/src/App.js
+++ beers_front_end/src/App.js
@@ -4,36 +4,61 @@ import { useState, useEffect } from "react";
 
 function App() {
 const url = 'http://localhost:3000/beers'
-const [data, setData] = useState([]);
+const [beerData, setBeerData] = useState([]);
 useEffect(() => {
   // if (!query) return;
 
   const fetchData = async () => {
     
     const response = await fetch(url, { method: "get" });
-    const data = await response.json();
-    setData(data);
+    const beerData = await response.json();
+    setBeerData(beerData);
     
   };
 
   fetchData();
 }, [url]);
-  
+const [formInputs, updateFormInputs] = useState({  brand: '', style: '',name: '', flavor:'', abv:''});
+const handleSubmit  = async (event) =>{
+  event.preventDefault()
+  try{
+    const response = await fetch('http://localhost:3000/beers', {
+      body: JSON.stringify(formInputs),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    updateFormInputs({
+      brand: '',
+      style: '',
+      name: '',
+      flavor:'',
+      abv:''
+    })
+    console.log(data)
+    // setNotices([data, ...notices])
+  }catch(error){
+    console.error(error)
+  }
+}
 return (
     <div className="beerform">
-      `${JSON.stringify(data)}`
+      `${JSON.stringify(beerData)}`
       <h4>Post your favorite beer</h4>
-    <form>
+    <form onSubmit= {handleSubmit}>
       <label htmlFor="brand">brand</label>
-      <input type="text" id="brand" />
+      <input type="text" onChange={(event)=>updateFormInputs({...formInputs, brand:event.target.value})}/>
       <label htmlFor="style">Style</label>
-      <input type="text" id="style" />
+      <input type="text" onChange={(event)=>updateFormInputs({...formInputs, style:event.target.value})}/>
       <label htmlFor="name">name</label>
-      <input type="text" id="name" />
+      <input type="text" onChange={(event)=>updateFormInputs({...formInputs, name:event.target.value})}/>
       <label htmlFor="flavor">flavor</label>
-      <input type="text" id="flavor" />
+      <input type="text" onChange={(event)=>updateFormInputs({...formInputs, flavor:event.target.value})}/>
       <label htmlFor="abv">abv</label>
-      <input type="text" id="abv" />
+      <input type="text" onChange={(event)=>updateFormInputs({...formInputs, abv:event.target.value})}/>
       <input type="submit" className="submit" />
     </form>
     </div>
